64aaec2ba99a4f1c4e1dc15f3afd49b7c67eac99
ctx2002 <ctx2002@gmail.com>
2020-04-17 12:18:42 +1200

    Update wicked_php.php

--- wicked_php.php
+++ wicked_php.php
@@ -2,3 +2,33 @@
 
 //decode base64 string
 echo file_get_contents('data://text/plain;base64,SSBsb3ZlIFBIUAo=');
+
+/**
+ * Encode data to Base64URL
+ * @param string $data
+ * @return boolean|string
+ */
+function base64url_encode($data)
+{
+  $b64 = base64_encode($data);
+
+  if ($b64 === false) {
+    return false;
+  }
+  // Convert Base64 to Base64URL by replacing “+” with “-” and “/” with “_”
+  $url = strtr($b64, '+/', '-_');
+  // Remove padding character from the end of line and return the Base64URL result
+  return rtrim($url, '=');
+}
+
+/**
+ * Decode data from Base64URL
+ * @param string $data
+ * @param boolean $strict
+ * @return boolean|string
+ */
+function base64url_decode($data, $strict = false)
+{
+  $b64 = strtr($data, '-_', '+/');
+  return base64_decode($b64, $strict);
+}
