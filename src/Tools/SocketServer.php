b09e696cac0b32f8f729e1573d5bd5cf5cba1822
ctx2002 <ctx2002@gmail.com>
2020-05-05 09:26:05 +1200

    Update SocketServer.php

--- src/Tools/SocketServer.php
+++ src/Tools/SocketServer.php
@@ -68,6 +68,10 @@ class SocketServer
                             socket_write($client,$output,4096);
                         }
                     } else {
+                        /* 
+                        socket_recv returns FALSE if there is no data and 0 if the socket is widowed 
+                        (disconnected by remote side)
+                        ***/
                         $bytes = socket_recv($socket, $buffer, 4096, 0);
 
                         if ($bytes == 0) {
@@ -103,3 +107,29 @@ return "input is: " . $input . "\n";
 $server = new SocketServer("127.0.0.1", 5789);
 $server->run($response);
  ****/
+
+/*
+<?php
+$er = error_reporting(0);
+$bytes    = socket_recv($socket,$buffer,1,MSG_WAITALL);
+error_reporting($er);
+
+// MEGA BUG HERE
+// this statuses are wrong and swapped, closed socket must be with "FALSE"
+// but in fact he swap the values:
+// http://php.net/manual/en/function.socket-recv.php
+//
+if($bytes===false){ // no data available, socket not closed
+    echo 'WS_READ_ERR1: '.socket_strerror(socket_last_error($socket)).PHP_EOL;
+    // print when no data available:
+    // WS_READ_ERR1: Resource temporarily unavailable
+    continue;
+}else if($bytes===0){ // socket closed
+    echo 'WS_READ_ERR2: '.socket_strerror(socket_last_error($socket)).PHP_EOL;
+    // print when socket closed:
+    // WS_READ_ERR2: Success
+    $process->close();
+}
+
+
+****/
